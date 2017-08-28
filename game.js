f73cc3502d571b96166f9e4cdc95dcae6f038fad
hanaxlee <30877949+hanaxlee@users.noreply.github.com>
2017-08-28 10:42:50 -1000

    Add files via upload

---
+++ game.js
@@ -0,0 +1,8 @@
+var matthew = {
+	game: "NBA",
+  age: 16,
+  grade: 12
+}
+var x = prompt("Give a game");
+matthew.game = x;
+window.alert(matthew.game);
---
+++ game1.js
@@ -0,0 +1,7 @@
+var x = prompt("Type game, age, or grade");
+var matthew = {
+	game: "NBA",
+  age: 16,
+  grade: 12
+}
+window.alert(matthew[x]);
