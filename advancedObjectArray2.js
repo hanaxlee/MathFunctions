ee384e6e808bc379d59ee9206c62a9809187381c
hanaxlee <30877949+hanaxlee@users.noreply.github.com>
2017-08-30 10:51:05 -1000

    Adding amounts
    Used var y = parseInt(x);

---
+++ advancedObjectArray2.js
@@ -0,0 +1,6 @@
+var bankAccount1 = {};
+var x = prompt("How much do you want to put in?");
+var y = parseInt(x);
+bankAccount1["name"] = "Tony";
+bankAccount1["balance"] = 100;
+window.alert(bankAccount1.balance + y);
